f65db49746cbdbb0208096636948b5391dfc8cfa
Dylan-IC-16 <d.rodriguez.6@estudiantec.cr>
2026-09-23 12:45:21 -0600

    Hola mundo

---
+++ ColoniaMarte/src/main/java/com/mycompany/coloniamarte/ColoniaMarte.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.coloniamarte;
+
+/**
+ *
+ * @author dylnr
+ */
+public class ColoniaMarte {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
